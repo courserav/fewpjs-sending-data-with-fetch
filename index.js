d342d7573367c721f40b8ee5bc7309fa79c41857
courserav <lvsmlit@gmail.com>
2021-08-13 03:44:00 -0400

    commit tree 9152c4c5907d188d0ef972dbac34899b113081f8

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(res => res.json())
+    .then(json => {
+        let idDiv = document.createElement("div")
+        idDiv.appendChild(document.createTextNode(json.id))
+        document.body.appendChild(idDiv)
+    })
+    .catch(function(error){
+        document.body.appendChild(document.createElement("div").appendChild(document.createTextNode(error.message)))
+    })
+}
